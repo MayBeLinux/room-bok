bfb372d94593e8031c9e79d180e8af8f3fbdcb23
MayBeLinux <joshutinel@gmail.com>
2026-06-04 14:03:07 +0200

    🔧 Fix datasource config file

--- backend/src/db/data-source.ts
+++ backend/src/db/data-source.ts
@@ -5,11 +5,12 @@ dotenv.config()
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: String(process.env.DB_HOST),
+    port: Number(process.env.DB_PORT),
+    username: String(process.env.DB_USERNAME),
+    password: String(process.env.DB_PASSWORD),
+    database: String(process.env.DB_DATABASE),
+    ssl: { rejectUnauthorized: false },
     synchronize: false,
     logging: true,
     entities: ["src/entity/*.ts"],
