11b956c0040215b622cf573222ec22958ee7b124
MayBeLinux <joshutinel@gmail.com>
2026-06-06 10:34:44 +0200

    remove white space

--- backend/src/routes/role.routes.ts
+++ backend/src/routes/role.routes.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express'; 
+import { Router, Request, Response } from 'express';
 
 // Import Controller
 import { roleController } from '../controllers/roleController';
