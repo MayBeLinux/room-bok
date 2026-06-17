e919dee47f0a66e0d01afbaf2bb3082d4d2e002a
MayBeLinux <joshutinel@gmail.com>
2026-06-17 15:34:43 +0200

    ✨ Define predefined views

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import SelectField from '../components/SelectField.vue'
+
+const selectedBuilding = ref<string | null>(null)
+</script>
+
+<template>
+  <div class="min-h-screen bg-background p-8">
+    <SelectField :label="selectedBuilding ?? 'Select Building'" />
+  </div>
+</template>
