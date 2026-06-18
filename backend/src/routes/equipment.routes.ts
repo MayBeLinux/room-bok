4ee8bed7d5ab3db71ffa4ac2b34d6ed775daa793
MayBeLinux <joshutinel@gmail.com>
2026-06-18 10:27:42 +0200

    🗃️ Connect the route

--- backend/src/routes/equipment.routes.ts
+++ backend/src/routes/equipment.routes.ts
@@ -6,9 +6,7 @@ import { equipmentController } from '../controllers/equipmentController';
 const router = Router();
 
 router.get('/equipments', equipmentController.listEquipments);
-router.get('/equipments/:id', equipmentController.getEquipment);
-router.post('/equipments', equipmentController.createEquipment);
-router.delete('/equipments/:id', equipmentController.deleteEquipment);
-router.put('/equipments/:id', equipmentController.updateEquipment);
-
+router.post('/equipment', equipmentController.createEquipment);
+router.delete('/equipment/:id', equipmentController.deleteEquipment);
+router.put('/equipment/:id', equipmentController.updateEquipment);
 export default router;
